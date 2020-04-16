e920f30aa69416aa7883b5d063dbf0f077613256
OldPreacher <62445437+OldPreacher@users.noreply.github.com>
2020-04-16 16:25:12 +0300

    calc

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,18 @@ using System.Windows.Shapes;
 
 namespace lab12
 {
-   
+
+    
     public partial class MainWindow : Window
     {
+        char flag;
+
         public MainWindow()
         {
+            flag = 'D';
             InitializeComponent();
         }
-
+            
 
         private void Button_ClickSys(object sender, RoutedEventArgs e)
         {
@@ -32,19 +36,109 @@ namespace lab12
             switch (clickedButton.Name)
             {
                 case "BtnHEX":
-                 
+                    switch(flag)
+                    {
+                        case 'D':
+                            if (flag == 'B') textBlock.Text = Convert.ToString(calc.decTohex(Convert.ToInt32(textBlock.Text)));
+                            flag = 'H';
+                            break;
+
+                        case 'B':
+                            if (flag == 'B')
+                            {
+                                textBlock.Text = Convert.ToString(calc.toDec(Convert.ToInt32(textBlock.Text), 2));
+                                textBlock.Text = Convert.ToString(calc.decTohex(Convert.ToInt32(textBlock.Text)));
+                            }
+                            flag = 'H';
+                            break;
+
+                        case 'O':
+                            if (flag == 'O')
+                            {
+                                textBlock.Text = Convert.ToString(calc.toDec(Convert.ToInt32(textBlock.Text), 8));
+                                textBlock.Text = Convert.ToString(calc.decTohex(Convert.ToInt32(textBlock.Text)));
+                            }
+                            flag = 'H';
+                            break;
+                    }   
                     break;
 
                 case "BtnBIN":
-                    textBlock.Text = Convert.ToString(calc.toDec(Convert.ToInt32(textBlock.Text), 2));
+                    switch(flag)
+                    {
+                        case 'D':
+                            if (flag == 'D') textBlock.Text = Convert.ToString(calc.decTo(Convert.ToInt32(textBlock.Text), 2));
+                            flag = 'B';
+                            break;
+
+                        case 'O':
+                            if (flag == 'O')
+                            {
+                                textBlock.Text = Convert.ToString(calc.toDec(Convert.ToInt32(textBlock.Text), 8));
+                                textBlock.Text = Convert.ToString(calc.decTo(Convert.ToInt32(textBlock.Text), 2));
+                            }
+                            flag = 'B';
+                            break;
+
+                        case 'H':
+                            if (flag == 'H')
+                            {
+                                textBlock.Text = Convert.ToString(calc.hexToDec(textBlock.Text));
+                                textBlock.Text = Convert.ToString(calc.decTo(Convert.ToInt32(textBlock.Text), 2));
+                            }
+                            flag = 'B';
+                            break;
+                    }
+
                     break;
 
                 case "BtnOCT":
+                    switch(flag)
+                    {
+                        case 'D':
+                            if(flag == 'D') textBlock.Text = Convert.ToString(calc.decTo(Convert.ToInt32(textBlock.Text), 8));
+                            flag = 'O';
+                            break;
 
+                        case 'B':
+                            if (flag == 'B')
+                            {
+                                textBlock.Text = Convert.ToString(calc.toDec(Convert.ToInt32(textBlock.Text), 2));
+                                textBlock.Text = Convert.ToString(calc.decTo(Convert.ToInt32(textBlock.Text), 8));
+                            }
+                            flag = 'O';
+                            break;
+
+                        case 'H':
+                            if (flag == 'B')
+                            {
+                                textBlock.Text = Convert.ToString(calc.hexToDec(textBlock.Text));
+                                textBlock.Text = Convert.ToString(calc.decTo(Convert.ToInt32(textBlock.Text), 8));
+                            }
+                            flag = 'O';
+                            break;
+                    }
                     break;
 
                 case "BtnDEC":
-              
+                    
+                    switch(flag)
+                    {
+                        case 'B':
+                            if (flag == 'B') textBlock.Text = Convert.ToString(calc.toDec(Convert.ToInt32(textBlock.Text), 2));
+                            flag = 'D';
+                            break;
+
+                        case 'O':
+                            if (flag == 'O') textBlock.Text = Convert.ToString(calc.toDec(Convert.ToInt32(textBlock.Text), 8));
+                            flag = 'D';
+                            break;
+                        case 'H':
+                            if (flag == 'H') textBlock.Text = Convert.ToString(calc.hexToDec(textBlock.Text));
+                            flag = 'D';
+                            break;
+                    }
+
                     break;
 
                 default:
@@ -56,6 +150,20 @@ namespace lab12
             Button clickedButton = (Button)sender;
             switch (clickedButton.Name)
             {
+                case "BtnClearLine":
+                    textBlock.Text = "";
+                    break;
+
+                case "BtnDelchar":         
+                    int lenght = textBlock.Text.Length - 1;
+                    string text = textBlock.Text;
+                    textBlock.Text = "";
+                        for (int i = 0; i < lenght; i++)
+                        {
+                        textBlock.Text = textBlock.Text + text[i];
+                        }                   
+                    break;
+
                 case "BtnNum1":
                     textBlock.Text += "1";
                     break;
